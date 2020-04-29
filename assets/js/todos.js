7d8e2420a4f8e0dcdca4cfa5f29ad7e4ed3c47e3
Shubham Saha <saha.helinski@gmail.com>
2020-04-29 15:33:52 +0530

    styling complete; app complete

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -15,6 +15,10 @@ $('input[type="text"]').keypress(function() {
   if (event.which === 13) {
     var todoText = $(this).val();
     $(this).val("");
-    $('ul').append("<li><span>X</span> " + todoText + "</li>");
+    $('ul').append("<li><span><i class=\"fas fa-trash\"></i></span> " + todoText + "</li>");
   }
+});
+
+$('.fa-edit').click(function() {
+  $('input[type="text"]').fadeToggle();
 });
